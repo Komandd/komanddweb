0f6be8fd1816062403600180d9889566553d0b28
Lautaro <lautarogorosito05gmail.com>
2023-02-28 02:34:22 +0100

    first commit

---
+++ cv3.js
@@ -0,0 +1,7 @@
+function saludar(nombre){
+    console.log("Hola " + nombre + ", espero que mi cv sea de su agrado. Saludos!");
+    alert ("Hola " + nombre + ", espero que mi cv sea de su agrado. Saludos!")
+}
+
+
+
